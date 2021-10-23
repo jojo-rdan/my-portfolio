5e77e568e058bfbfa1ce046c208e65a66cd5a554
Jordan <jordan.olaves91@gmail.com>
2021-10-22 21:39:06 -0500

    MyPortfolio

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,8 @@
 export default function Home () {
     return ( 
         <div>
-            Este será el home, con Proyectos, Experiencia y Contacto
+            Este será el home, con Proyectos, Experiencia y Contacto.
+            <p>Línea temporal</p>
         </div>
      );
 }

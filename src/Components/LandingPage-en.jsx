a2b809ee87cd0d9c238ad3a1f02c7fe844b807d1
Jordan <jordan.olaves91@gmail.com>
2021-11-03 21:02:04 -0500

    MyPortfolio

--- src/Components/LandingPage-en.jsx
+++ src/Components/LandingPage-en.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import image from '../img/IMG_3978.jpg';
+import spa from '../img/espana.png';
 
-export default function LandingPageEn () {
-    return ( 
+export default function LandingPageEn() {
+    return (
         <div>
             <Link to="/">
-                <button>Banderita de SPA</button>
+                <img src={spa} width={'30px'} alt="Flag of Spain" />
             </Link>
             This will be my landing, with my profile pic, cool Vanta.js background and enter.
-            <div>
-                <img src={image} width={'250px'} alt='imagen de perfil' />
-            </div>
+                <div>
+                    <img src={image} width={'250px'} alt='imagen de perfil' />
+                    <h1>Hi! My name is Jordan González</h1>
+                </div>
+                <div>
+                    <h1>Desarrollador Web Full-Stack</h1>
+                    <p>I am very passionate about learning and contributing ideas to create quality projects.</p>
+                </div>
             <Link to="/home-en">
-                <button>Enter</button>
+                <button>About me!</button>
             </Link>
         </div>
-     );
+    );
 }
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import image from '../img/IMG_3978.jpg';
+import us from '../img/estados-unidos.png';
 
 export default function LandingPage () {
     return ( 
         <div>
             <Link to="/-en">
-                <button>Banderita de US</button>
+                <img src={us} width={'30px'} alt="Bandera de USA" />
             </Link>
             Este será mi Landing, con mi foto de perfil, vanta genial.js fondo y entrar.
-            <div>
-                <img src={image} width={'250px'} alt='imagen de perfil' />
-            </div>
+                <div>
+                    <img src={image} width={'250px'} alt='imagen de perfil' />
+                    <h1>Hola! Mi nombre es Jordan González</h1>
+                </div>
+                <div>
+                    <h1>Desarrollador Web Full-Stack</h1>
+                    <p>Soy un apasionado de aprender y aportar ideas para crear proyectos de calidad.</p>
+                </div>
             <Link to="/home">
-                <button>Entrar</button>
+                <button>Conóceme!</button>
             </Link>
         </div>
      );
